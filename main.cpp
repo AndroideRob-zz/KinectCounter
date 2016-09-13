0eacfe156e49d7a839d9fda610be886fc4b4a378
AndroideRob <robertas.konarskis@gmail.com>
2016-09-13 13:56:48 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,9 +7,68 @@
 ***********************************
 **********************************/
 
-int main() {
-    while(1) {
-        
-    } 
-    return 0;
+#include "stdafx.h"
+#include "kinect2_grabber2.h"
+
+#include <pcl/console/parse.h>
+#include <pcl/point_types.h>
+#include <pcl/visualization/pcl_visualizer.h>    
+#include <pcl/sample_consensus/sac_model_plane.h>
+#include <pcl/people/ground_based_people_detection_app.h>
+
+typedef pcl::PointXYZ PointType;
+
+int main(int argc, char* argv[])
+{
+	// PCL Visualizer
+	boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(
+		new pcl::visualization::PCLVisualizer("Point Cloud Viewer"));
+	viewer->setCameraPosition(0.0, 0.0, -2.5, 0.0, 0.0, 0.0);
+
+	// Point Cloud
+	pcl::PointCloud<PointType>::ConstPtr cloud;
+
+	// Retrieved Point Cloud Callback Function
+	boost::mutex mutex;
+	boost::function<void(const pcl::PointCloud<PointType>::ConstPtr&)> function =
+		[&cloud, &mutex](const pcl::PointCloud<PointType>::ConstPtr& ptr) {
+		boost::mutex::scoped_lock lock(mutex);
+
+		/* Point Cloud Processing */
+		cout << "points received" << endl;
+
+		cloud = ptr->makeShared();
+	};
+
+	// Kinect2Grabber
+	boost::shared_ptr<pcl::Grabber> grabber = boost::make_shared<pcl::Kinect2Grabber>();
+
+	// Register Callback Function
+	boost::signals2::connection connection = grabber->registerCallback(function);
+
+	// Start Grabber
+	grabber->start();
+
+	while (!viewer->wasStopped()) {
+		// Update Viewer
+		viewer->spinOnce();
+
+		boost::mutex::scoped_try_lock lock(mutex);
+		if (lock.owns_lock() && cloud) {
+			// Update Point Cloud
+			if (!viewer->updatePointCloud(cloud, "cloud")) {
+				viewer->addPointCloud(cloud, "cloud");
+			}
+		}
+	}
+
+	// Stop Grabber
+	grabber->stop();
+
+	// Disconnect Callback Function
+	if (connection.connected()) {
+		connection.disconnect();
+	}
+
+	return 0;
 }
