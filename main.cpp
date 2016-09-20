7f8ead7998299dbafca13a3a69f51ad972e10af6
AndroideRob <robertas.konarskis@gmail.com>
2016-09-20 14:05:44 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,3 @@
-
-/**********************************
-***********************************
-*
-* EXPO GROUP 12 MAIN.CPP
-*
-***********************************
-**********************************/
-
 #include "stdafx.h"
 #include "kinect2_grabber2.h"
 
@@ -16,17 +7,112 @@
 #include <pcl/sample_consensus/sac_model_plane.h>
 #include <pcl/people/ground_based_people_detection_app.h>
 
+#include <pcl/ModelCoefficients.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/segmentation/extract_clusters.h>
+
+#include <pcl/compression/octree_pointcloud_compression.h>
+
 typedef pcl::PointXYZ PointType;
+
+int objectsCount = 0; // number of objects
+
+/* Extract clusters and print objects count */
+pcl::PointCloud<PointType>::Ptr extractClusters(pcl::PointCloud<PointType>::Ptr cloud) {
+	
+	/* THIS IS WHERE THE FUN STARTS */
+	std::cout << "PointCloud before filtering has: " << cloud->points.size() << " data points." << std::endl; //*
+
+	// idk what it does
+	cloud->points.shrink_to_fit();
+	
+	// Create the filtering object: downsample the dataset using a leaf size of 1cm
+	pcl::VoxelGrid<PointType> vg;
+	pcl::PointCloud<PointType>::Ptr cloud_filtered(new pcl::PointCloud<PointType>);
+	vg.setInputCloud(cloud);
+	vg.setLeafSize(0.01f, 0.01f, 0.01f);
+	vg.filter(*cloud_filtered);
+	std::cout << "PointCloud after filtering has: " << cloud_filtered->points.size() << " data points." << std::endl; 
+
+	// Create the segmentation object for the planar model and set all the parameters
+	pcl::SACSegmentation<pcl::PointXYZ> seg;
+	pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
+	pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
+	pcl::PointCloud<PointType>::Ptr cloud_plane(new pcl::PointCloud<PointType>());
+	pcl::PCDWriter writer;
+	seg.setOptimizeCoefficients(true);
+	seg.setModelType(pcl::SACMODEL_PLANE);
+	seg.setMethodType(pcl::SAC_RANSAC);
+	seg.setMaxIterations(3);
+	seg.setDistanceThreshold(0.08);
+
+	int i = 0, nr_points = (int)cloud_filtered->points.size();
+	while (cloud_filtered->points.size() > 0.1 * nr_points)
+	{
+		// Segment the largest planar component from the remaining cloud
+		seg.setInputCloud(cloud_filtered);
+		seg.segment(*inliers, *coefficients);
+		if (inliers->indices.size() == 0)
+		{
+			std::cout << "Could not estimate a planar model for the given dataset." << std::endl;
+			break;
+		}
+
+		// Extract the planar inliers from the input cloud
+		pcl::ExtractIndices<PointType> extract;
+		extract.setInputCloud(cloud_filtered);
+		extract.setIndices(inliers);
+		extract.setNegative(false);
+
+		// Get the points associated with the planar surface
+		extract.filter(*cloud_plane);
+
+		// Remove the planar inliers, extract the rest
+		extract.setNegative(true);
+		extract.filter(*cloud);
+		*cloud_filtered = *cloud;
+	}
+
+	// Creating the KdTree object for the search method of the extraction
+	pcl::search::KdTree<PointType>::Ptr tree(new pcl::search::KdTree<PointType>);
+	tree->setInputCloud(cloud_filtered);
+
+	std::vector<pcl::PointIndices> cluster_indices;
+	pcl::EuclideanClusterExtraction<PointType> ec;
+	ec.setClusterTolerance(0.05);
+	ec.setMinClusterSize(300);
+	ec.setMaxClusterSize(25000);
+	ec.setSearchMethod(tree);
+	ec.setInputCloud(cloud_filtered);
+	ec.extract(cluster_indices);
+
+	int j = 0;
+	for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin(); it != cluster_indices.end(); ++it)
+	{
+		j++;
+	}
+
+	objectsCount = j;
+	return cloud;
+}
 
 int main(int argc, char* argv[])
 {
+
 	// PCL Visualizer
 	boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(
 		new pcl::visualization::PCLVisualizer("Point Cloud Viewer"));
 	viewer->setCameraPosition(0.0, 0.0, -2.5, 0.0, 0.0, 0.0);
 
 	// Point Cloud
-	pcl::PointCloud<PointType>::ConstPtr cloud;
+	pcl::PointCloud<PointType>::Ptr cloud;
 
 	// Retrieved Point Cloud Callback Function
 	boost::mutex mutex;
@@ -38,6 +124,9 @@ int main(int argc, char* argv[])
 		cout << "points received" << endl;
 
 		cloud = ptr->makeShared();
+
+		// Extracting the clusters and changing the cloud object
+		extractClusters(cloud);	
 	};
 
 	// Kinect2Grabber
@@ -56,8 +145,14 @@ int main(int argc, char* argv[])
 		boost::mutex::scoped_try_lock lock(mutex);
 		if (lock.owns_lock() && cloud) {
 			// Update Point Cloud
+		
 			if (!viewer->updatePointCloud(cloud, "cloud")) {
 				viewer->addPointCloud(cloud, "cloud");
+			}
+
+			// Updating label with number of objects (lower left corner)
+			if (!viewer->updateText("Objects on screen: " + std::to_string(objectsCount), 20, 20, "textId")) {
+				viewer->addText("Objects on screen: " + std::to_string(objectsCount), 20, 20, "textId");
 			}
 		}
 	}
